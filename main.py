ed4ff1cefe7a1fbc6a26b9ac5120bbd757306868
andresshady <138407786+andresshady@users.noreply.github.com>
2025-08-29 01:10:15 -0600

    adding advances

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
-""" this project is created by Andres Sanchez.
-The project consists of electronics support for designing app
-"""
-#import math
+import Circuit
 
-class Circuit:
-    def __init__(self,resistance,voltage,current):
-        self.resistance = resistance
-        self.voltage = voltage
-        self.current = current
-        self.resistor = {}
-        self.capacitor = {}
-        self.inductor = {}
+circuit = Circuit.Circuit(12)
 
-    def add_resistor(self,name,value):
-        self.resistor[name] = value
-
-    def add_capacitor(self,name,value):
-        self.capacitor[name] = value
-
-    def add_inductor(self,name,value):
-        self.inductor[name] = value
-
-    def get_resistors(self):
-        return self.resistor
-
-    def get_capacitors(self):
-        return self.capacitor
-
-    def get_inductors(self):
-        return self.inductor
-
-    def calculate_voltage(self):
-        return self.current * self.resistance
-
-    def calculate_current(self):
-        return self.voltage / self.resistance
-
-    def calculate_power(self):
-        return self.voltage * self.current
-
-
+resistors = circuit.add_resistor(input("assig a resistor tag: "), int(input("add a resistor value: ")))
+current = circuit.calculate_tcurrent()
+resistance = circuit.calculate_tresistance()
+print(circuit.get_resistors())
+print(current)
 
 
 
